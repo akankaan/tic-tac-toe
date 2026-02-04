ceaca68201b71a39c7d14b5382f413b840074122
unpicasso <kaan1286@yahoo.com>
2026-02-04 17:57:59 -0500

    Started implementation

---
+++ imperfect-opponent.py
@@ -0,0 +1,11 @@
+
+
+board = [[0, 0, 0], # 0 denotes empty spot, 1 is "X", 2 is "O"
+         [0, 0, 0],
+         [0, 0, 0]]
+
+
+WINNING_POS = [[0, 1, 2], [3, 4, 5], [6, 7, 8]  # rows
+               [0, 3, 6], [1, 4, 7], [2, 5, 8], # columns
+               [0, 4, 8], [2, 4, 6]]            # diagonals
+
